1a08fec48e29b7328132305b3c2e527f3de83634
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-10-19 17:55:09 +0300

    sky is the limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,31 +1,11 @@
-# 0-the_sky_is_the_limit_not.pp
+# Increases amount of traffic nginx can handle
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
 
-# Install Nginx package (if not already installed)
-package { 'nginx':
-  ensure => 'installed',
-}
-
-# Define an Nginx site configuration
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => template('path/to/your/nginx_config.erb'),
-}
-
-# Create a symbolic link to enable the site
-file { '/etc/nginx/sites-enabled/default':
-  ensure  => link,
-  target  => '/etc/nginx/sites-available/default',
-  require => File['/etc/nginx/sites-available/default'],
-}
-
-# Reload Nginx to apply the changes
-exec { 'reload-nginx':
-  command     => '/usr/sbin/service nginx reload',
-  refreshonly => true,
-  subscribe   => File['/etc/nginx/sites-enabled/default'],
-}
-
-# Notify that the changes are completed
-notify { 'Nginx configuration updated':
-  require => Exec['reload-nginx'],
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
